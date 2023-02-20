29aca2897ce13387dd45768c0ff177b1246d5fc2
ivanovv <aa2652839@gmail.com>
2023-02-20 13:58:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/petstore/Login_window.java
@@ -0,0 +1,14 @@
+package com.example.petstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_window extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_window);
+    }
+}
