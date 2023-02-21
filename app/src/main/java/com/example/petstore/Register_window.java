633a34e8d0be1b86d00d633c4769e9f19538a687
ivanovv <aa2652839@gmail.com>
2023-02-21 11:49:52 +0900

    123

---
+++ app/src/main/java/com/example/petstore/Register_window.java
@@ -0,0 +1,14 @@
+package com.example.petstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_window extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_window);
+    }
+}
